6fce0568d93e0290f54c7ee0afbd853077ee022b
shauntanwj <shauntanwj10@gmail.com>
2023-09-25 19:03:33 +0800

    Build sidebar for displaying genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,16 +14,16 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="10px"
-        spacing={10}
+        spacing={3}
       >
         {isLoading
           ? skeletons.map((skeleton) => (
-              <GameCardContainer>
+              <GameCardContainer key={skeleton}>
                 <GameCardSkeleton key={skeleton} />
               </GameCardContainer>
             ))
           : data.map((game) => (
-              <GameCardContainer>
+              <GameCardContainer key={game.id}>
                 <GameCard key={game.id} game={game} />
               </GameCardContainer>
             ))}
